97d5a1a6c6c132f4e3e6a933dd5eacf0e74bc79e
Kouzi99 <125484339+Kouzi99@users.noreply.github.com>
2024-01-26 17:09:13 +0100

    photo pa

--- scripts.js
+++ scripts.js
@@ -4,27 +4,27 @@ const products = [
     {
     name: 'dress',
     price: 150,
-    photoLink: '/foto/7.jpg',
+    photoLink: '/olka/foto/7.jpg',
     },
     {
     name: 'skirt',
     price: 80,
-    photoLink: '/foto/8.jpg',
+    photoLink: '/olka/foto/8.jpg',
     },
     {
     name: 'dress',
     price: 160,
-    photoLink: '/foto/9.jpg',
+    photoLink: '/olka/foto/9.jpg',
     },
     {
     name: 'dress',
     price: 120, 
-    photoLink: '/foto/10.jpg',
+    photoLink: '/olka/foto/10.jpg',
     },
     {
     name: 'dress',
     price: 140,
-    photoLink: '/foto/11.jpg',
+    photoLink: '/olka/foto/11.jpg',
     },    
     ];
 //display product details
