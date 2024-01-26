133adafe4d845ad4739125557a94409a349bcbe4
Kouzi99 <125484339+Kouzi99@users.noreply.github.com>
2024-01-26 16:06:28 +0100

    new

---
+++ src/scripts.js
@@ -0,0 +1,88 @@
+
+// Product object
+const products = [
+    {
+    name: 'dress',
+    price: 150,
+    size: 'Large',
+    photoLink: '../foto/7.jpg',
+    },
+    {
+    name: 'skirt',
+    price: 80,
+    photoLink: '../foto/8.jpg',
+    },
+    {
+    name: 'dress',
+    price: 160,
+    photoLink: '../foto/9.jpg',
+    },
+    {
+    name: 'dress',
+    price: 120, 
+    photoLink: '../foto/10.jpg',
+    },
+    {
+    name: 'dress',
+    price: 140,
+    photoLink: '../foto/11.jpg',
+    },    
+    ];
+//display product details
+function displayProductDetails(products) {
+    const productContainer = document.getElementById('product-container');
+    // Loop through each product in the array
+    products.forEach(product => {
+        // Create a product card div
+        const productCard = document.createElement('div');
+        productCard.className = 'product-card';
+        // photo
+        const productImage = document.createElement('img');
+        productImage.setAttribute('loading', 'lazy');
+        productImage.src = product.photoLink;
+        productImage.alt = product.name;
+        // Create HTML
+        const productName = document.createElement('h2');
+        productName.textContent = product.name;
+        productName.style.fontFamily = "nanum-myeongjo-bold";
+        productName.style.fontSize = "19px";
+        // price
+        const productPrice = document.createElement('p');
+        productPrice.textContent = `${product.price} €`;
+        // Append 
+        productCard.appendChild(productImage);
+        productCard.appendChild(productName);
+        productCard.appendChild(productPrice);
+        // Append the product card 
+        productContainer.appendChild(productCard);
+    });
+}
+displayProductDetails(products);
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
